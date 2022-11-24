ee3b753eb2155ca3bf3aa1225fb4b6a0e1ddb77f
Mario Kozjak <kozjakm1@gmail.com>
2022-11-24 17:01:06 +0100

    add 'not supported' fixme

--- console.aws.amazon.com.js
+++ console.aws.amazon.com.js
@@ -1,4 +1,5 @@
 if (window.top === window) {
+    // FIXME: not supported on s3, iam, systems manager, cloudwatch, acm...
     if (document.querySelectorAll('nav[class*="_toggle_"]')[0].ariaHidden === "true") {
         document.querySelectorAll('span[class*="_close-button_"]')[0]
         .getElementsByTagName("button")[0]
