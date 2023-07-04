744fa22cdbd8c85e6960f4d4c1d9a675dd13fa71
Mario Kozjak <kozjakm1@gmail.com>
2023-07-04 16:11:31 +0200

    update asana
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- app.asana.com.js
+++ app.asana.com.js
@@ -1,6 +1,6 @@
 if (window.top === window) {
     const doc = document.querySelectorAll('[aria-label="Hide sidebar"], [aria-label="Expand sidebar"]')
 
-    if (doc.length === 1)
+    if (doc.length === 1 && document.readyState === "complete")
         doc[0].click()
 }
