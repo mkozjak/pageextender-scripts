265b7e35565013f08e17ff6fd8e2437c5d388148
Mario Kozjak <kozjakm1@gmail.com>
2023-08-15 21:47:36 +0200

    add sportshub.fan airplay stream picker
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- sportshub.fan.js
+++ sportshub.fan.js
@@ -1,3 +1,46 @@
 if (window.top === window) {
-	window.open = function() {};
+	let airPlayLink = null
+
+	document.addEventListener('keydown', function(event) {
+	    if (event.key === 'a') {
+	        // Use the saved link here
+	        console.log("OPEN", airPlayLink)
+	        window.open(airPlayLink, '_self')
+	    }
+	})
+
+	// add method to fetch me the first link which supports airplay
+	if (window.location.pathname.includes('/event/')) {
+		const linksBlock = document.getElementById('links_block')
+		getAirPlayLink(linksBlock)
+	}
+
+	if (window.location.pathname.includes('/embed77/')) {
+		const video = document.querySelector('video');
+
+		if (window.WebKitPlaybackTargetAvailabilityEvent) {
+		    video.addEventListener('webkitplaybacktargetavailabilitychanged', function(event) {
+		        if (event.availability === 'available') {
+		            const airPlayButton = document.createElement('button')
+		            airPlayButton.textContent = 'AirPlay'
+
+		            airPlayButton.addEventListener('click', function() {
+		                video.webkitShowPlaybackTargetPicker()
+		            })
+
+		            document.body.appendChild(airPlayButton)
+		        }
+		    })
+		}
+	}
+
+	function getAirPlayLink(element) {
+		if (element.tagName === 'A' && element.href && element.href.includes('embedstream.me')) {
+			airPlayLink = element.href
+			return
+		}
+
+		for (let i = 0; i < element.children.length; i++)
+			getAirPlayLink(element.children[i])
+	}
 }
