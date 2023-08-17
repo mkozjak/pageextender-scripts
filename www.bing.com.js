ed8873a02af361f9899f7b9afdef838f5452c7d3
Mario Kozjak <kozjakm1@gmail.com>
2023-08-17 10:34:53 +0200

    minor
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- www.bing.com.js
+++ www.bing.com.js
@@ -1,5 +1,7 @@
 if (window.top === window) {
     window.addEventListener("wheel", e => {
-        if (e.target.className.includes("cib-serp-main")) e.stopPropagation();
-    });
+        if (e.target.className.includes("cib-serp-main")) e.stopPropagation()
+    })
+
+    document.querySelector('[aria-label="Additional Results"]').remove()
 }
