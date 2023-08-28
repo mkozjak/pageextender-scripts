2067ed43f182bccc787fdf7f3476bb760e0e5b02
Mario Kozjak <kozjakm1@gmail.com>
2023-08-28 10:38:21 +0200

    Revert "fix asana leftmenu closing glitch"
    This reverts commit 1031d9c75bdd7fcbac4c63dd6c061d2be5713420.

--- app.asana.com.js
+++ app.asana.com.js
@@ -1,8 +1,6 @@
 if (window.top === window) {
-    setTimeout(function() {
-        const doc = document.querySelectorAll('[aria-label="Hide sidebar"], [aria-label="Expand sidebar"]')
+    const doc = document.querySelectorAll('[aria-label="Hide sidebar"], [aria-label="Expand sidebar"]')
 
-        if (doc.length === 1 && document.readyState === "complete")
-            doc[0].click()
-    }, 3000)
+    if (doc.length === 1 && document.readyState === "complete")
+        doc[0].click()
 }
