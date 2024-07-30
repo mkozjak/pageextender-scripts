454c4477b2baab21dd2317881faa5909a9d5c027
Mario Kozjak <kozjakm1@gmail.com>
2024-07-30 21:43:51 +0200

    remove margins from fallout.fandom.com
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- fallout.fandom.com.js
+++ fallout.fandom.com.js
@@ -1,4 +1,7 @@
 if (window.top === window) {
     document.getElementsByClassName("global-navigation")[0].remove()
     document.getElementsByClassName("page__right-rail")[0].remove()
+    document.getElementsByClassName("main-container")[0].style.margin = 0
+    document.getElementsByClassName("main-container")[0].style.width = "100%"
+    document.getElementsByClassName("fandom-community-header__background cover fullScreen ")[0].style.width = "100%"
 }
